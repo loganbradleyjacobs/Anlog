f414d0a82a09175408d153feca225411d99fdd10
lbj1533 <lxbxjx15@outlook.com>
2025-02-26 21:14:10 -0500

    Lexer basic functionality

--- tokenizer.py
+++ tokenizer.py
@@ -2,9 +2,11 @@ import tokenizer_functions as tokenizer_functions
 
 
 source_filepath = "int_prototype.anlog"
-split_operators = [" ","\n"]
+split_delimiters = " \n()"
 
 with open(source_filepath, 'r') as source_file:
    for code_line in source_file:
-      tokenizer_functions.code_cleaner(code_line)
-
+      code_line = tokenizer_functions.code_cleaner(code_line)
+      tokens = tokenizer_functions.split_by_delimiters(code_line, split_delimiters)
+      if tokens != None:
+         print(tokens)
--- tokenizer_functions.py
+++ tokenizer_functions.py
@@ -1,3 +1,5 @@
+import re
+
 def remove_comments(code_line):
    if code_line[:2] == "//":
       return ""
@@ -6,6 +8,16 @@ def remove_comments(code_line):
 
 def code_cleaner(code_line):
    code_line = remove_comments(code_line)
-   if code_line != "":
-      code_line = code_line.strip()
-      print(code_line, end = "")
+   code_line = code_line.strip()
+   if code_line == "":
+      return code_line
+   else:
+      return code_line + "\n"
+   
+def split_by_delimiters(text, delimiters):
+   tokenized_line = re.split(r'[ \n()]',text)
+   if tokenized_line != ['']:
+      return tokenized_line
+   else:
+      return None
+   
